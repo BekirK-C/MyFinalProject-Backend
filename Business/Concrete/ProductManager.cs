b300ccfa7391884cad39a67330707527bbc0919c
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-06 15:39:51 +0300

    Management layers

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productdal;
+
+        public ProductManager(IProductDal productdal)
+        {
+            _productdal = productdal;
+        }
+
+        public List<Product> GetAll()
+        {
+            return _productdal.GetAll();
+        }
+    }
+}
