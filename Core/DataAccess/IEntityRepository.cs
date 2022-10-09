fa2bea7ed085de4d8f80b48572a4343684de61fb
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-09 11:02:12 +0300

    Core Katmanı Giris

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,10 +1,11 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+// CORE KATMANI DİĞER AKTMANLARI REFERANS ALMAZ. CORE KATMANI EVRENSELDİR. TÜM .NET PROJEELRİNDE KULLANILABİLİR. 
+namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T : class, IEntity, new()
     {
