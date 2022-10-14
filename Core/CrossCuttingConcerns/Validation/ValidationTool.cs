5e9fd028c645f73c96ff954f25736f1267959c79
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-14 19:02:01 +0300

    Interceptors

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    // Bu tip tool'lar genelde static yapılır. Tek instance oluşturup sadece onu kullnabilmek için. Tekrar new'lemeye gerek kalmaz
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
