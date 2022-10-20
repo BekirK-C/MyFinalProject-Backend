74d2bd2a2ceead33e2f3d21ace592c853aecbe12
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-20 17:45:52 +0300

    Token Islemleri

---
+++ Core/Utilities/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Hashing
+{
+    public class HashingHelper
+    {
+        // Verilen password'ün hash'ini oluşturur.
+        public static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        // Sonradan sisteme giren kişinin verdiği password'ü veri kaynağındaki hash ile ilgili salt'a göre eşleşip eşleşmediği bilgisini verir.
+        public static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
