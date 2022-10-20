816c1f70055632270838d91ed914046cf9fe9433
BekirK-C <80921292+BekirK-C@users.noreply.github.com>
2022-10-20 21:37:26 +0300

    Auth İşlemleri

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
@@ -18,7 +19,13 @@ namespace Business.Constants
         public static string ProductUpdated = "Ürün Güncellendi";
         public static string ProductNameAlreadyExist = "Bu isimde başka bir ürün vardır.";
         public static string CategoryLimitExceded = "Kategori limitine ulaşılmıştır.";
-
         public static string AuthorizationDenied = "Erişim Reddedildi.";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+        public static string ProductNameAlreadyExists = "Ürün ismi zaten mevcut";
     }
 }
