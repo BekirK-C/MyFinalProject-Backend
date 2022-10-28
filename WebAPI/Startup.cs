d1398cc8f56b4fe21bd891e44724582a34ac9098
BekirK-C <b.b.kamac@gmail.com>
2022-10-28 19:54:08 +0300

    useCors

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -40,6 +40,7 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>();  // Eðer contructor'da IProductService çaðrýlýrsa arkaplanda ProductManager new'i verir.
             //services.AddSingleton<IProductDal, EfProductDal>();
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -67,6 +68,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
